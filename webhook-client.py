afad36aa03812201f97835508871b90ddcdefa08
WeChatPadPro <your-email@example.com>
2025-07-21 07:50:09 +0800

    fixwebhook
    fixwebhook

--- webhook-client.py
+++ webhook-client.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-WEBHOOK_SECRET = "your_secret_key"
+WEBHOOK_SECRET = "wh_sk_2024_a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0"
 INCLUDE_SELF_MESSAGE = True
 
 # 日志配置
